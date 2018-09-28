caca311694a19c7fd00c2b96ab0b32423f1d9d68
Solimar Silva <solimar.silva@trt8.jus.br>
2018-09-28 14:51:58 -0300

    excluido a libe Jose

--- src/main/java/br/com/solimar/rest/jwt/client/ClienteRest.java
+++ src/main/java/br/com/solimar/rest/jwt/client/ClienteRest.java
@@ -47,85 +47,13 @@ public class ClienteRest implements Serializable {
 
 	}
 
-	public List<PessoaVO> showallitems(String token) {
-
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target(SERVER_URI + ENTRY_POINT + "/showallitems");
-
-		Response response = null;
-		try {
-
-			response = target.request(MediaType.APPLICATION_JSON).header("token", token).get();
-
-		} catch (Exception e) {
-			System.out.println("Exception : " + e.getMessage());
-			e.printStackTrace();
-		}
-
-		System.out.println("Status: " + response.getStatus());
-		System.out.println("Status Info: " + response.getStatusInfo());
-		
-
-		List<PessoaVO> lista = new ArrayList<>();
-		if (response.getStatus() == 200) {
-			lista = response.readEntity(new GenericType<List<PessoaVO>>() {
-			});
-
-		}else{
-			System.out.println("readEntity: " + response.readEntity(String.class));
-		}
-
-		return lista;
-
-	}
-
-	public String authenticate() {
-
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target(SERVER_URI + ENTRY_POINT + "/authenticate");
-
-		Response response = null;
-		try {
-
-			response = target.request(MediaType.APPLICATION_JSON).header("username", "user").header("password", "123")
-					.get();
-
-		} catch (Exception e) {
-			System.out.println("Exception : " + e.getMessage());
-			e.printStackTrace();
-		}
-
-		System.out.println("Status: " + response.getStatus());
-		System.out.println("Status Info: " + response.getStatusInfo());
-		
-		
-		if(response.getStatus() == 200){
-			return response.readEntity(String.class);
-		}
-		else{
-			System.out.println("readEntity: " + response.readEntity(String.class));
-			return null;
-		}
-		
-
-	}
+	
 
 	public static void main(String[] args) {
 
 		ClienteRest cliente = new ClienteRest();
 
-		System.out.println("******** Authenticate **********************");
-		String token = cliente.authenticate();
-		
-		/*try {
-			Thread.sleep(120*1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-
-		System.out.println("******** List **********************");
-		List<PessoaVO> cars = cliente.showallitems(token);
+		List<PessoaVO> cars = cliente.list();
 		for (PessoaVO c : cars) {
 			System.out.println("Resposta: " + c.getNome() + " - " + c.getIdade());
 		}
